70c64d24257c36c21197c05258087ada8fca8964
JustOssa <rgsoussama@gmail.com>
2022-02-19 18:52:52 +0100

    Footer social icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,21 +1,53 @@
-import {
-    Container,
-    Text,
-    Link
-} from '@chakra-ui/react';
-
-
+import { Container, Text, Link, Icon, VStack, HStack } from '@chakra-ui/react';
+import { FaGithub, FaTwitter, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
 
     return (
-        <Container as="footer" maxW="xl" textAlign="center" py={10}>
-            <Text>
-                Made with ❤️ by{' '}
-                <Link href="https://github.com/JustOssa" isExternal>
-                    JustOssa
-                </Link>
-            </Text>
+        <Container as="footer" maxW="xl" py={10}>
+            <VStack>
+                <Text>
+                    Made with ❤️ by{' '}
+                    <Link href="https://github.com/JustOssa" isExternal>
+                        JustOssa
+                    </Link>
+                </Text>
+
+                <HStack spacing={4}>
+                    <Link
+                        isExternal
+                        rel="noreferrer"
+                        href={process.env.githubUrl}
+                        aria-label="Github Repo"
+                    >
+                    <Icon as={FaGithub} boxSize={4} />
+                    </Link>
+                    <Link
+                        isExternal
+                        rel="noreferrer"
+                        href={process.env.twitterUrl}
+                        aria-label="Twitter Profile"
+                    >
+                    <Icon as={FaTwitter} boxSize={4} />
+                    </Link>
+                    <Link
+                        isExternal
+                        rel="noreferrer"
+                        href={process.env.linkedinUrl}
+                        aria-label="Linkedin Profile"
+                    >
+                    <Icon as={FaLinkedin} boxSize={4} />
+                    </Link>
+                    <Link
+                        isExternal
+                        rel="noreferrer"
+                        href={`mailto:${process.env.emailAddress}`}
+                        aria-label="Email Address"
+                    >
+                    <Icon as={FaEnvelope} boxSize={4} />
+                    </Link>
+                </HStack>
+            </VStack>
         </Container>
     );
 }
