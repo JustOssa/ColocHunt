777617d4b3088637be22ac3eecfd87c9258d4ec5
JustOssa <rgsoussama@gmail.com>
2022-03-06 21:46:23 +0100

    Header colored

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,17 @@
 import {
     Box,
     Heading,
-    Text
+    Text,
+    chakra,
+    useColorModeValue
 } from '@chakra-ui/react';
 
 
 const Header = () => {
-
+    const logoColor = useColorModeValue("brand.600", "brand.400");
     return (
         <Box textAlign="center">
-            <Heading as="h1" size="3xl">Coloc V1</Heading>
+            <Heading as="h1" size="3xl">Coloc <chakra.span color={logoColor}>V1</chakra.span></Heading>
             <Text fontSize="lg" fontWeight="semibold" mt={2}>
                 Find well-matched roommates for free
             </Text>
