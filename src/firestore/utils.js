4b1ae0cc672e051f0635fd6b5dd20851930a8406
JustOssa <rgsoussama@gmail.com>
2022-03-29 18:05:26 +0000

    🔥 Firestore, rooms fetch

---
+++ src/firestore/utils.js
@@ -0,0 +1,60 @@
+import {
+    collection,
+    setDoc,
+    updateDoc,
+    getDocs,
+    getDoc,
+    doc,
+    deleteDoc,
+    query,
+    where,
+    orderBy
+  } from "firebase/firestore";
+import { db } from "../utils/firebase-config";
+    
+    // create user
+    export function createUser(userData, id) {
+        const userDoc = doc(db, "users", id);
+        return setDoc(userDoc, userData);
+    }
+    export function updateUser(userData, id) {
+        const userDoc = doc(db, "users", id);
+        return updateDoc(userDoc, userData);
+    }
+    
+    // delete user
+    export function deleteUser(id) {
+        const userDoc = doc(db, "users", id);
+        return deleteDoc(userDoc);
+    }
+
+    // get user
+    export function getUser(id) {
+        const userDoc = doc(db, "users", id);
+        return getDoc(userDoc);
+    }
+    
+    // get all users
+    export function getUsers() {
+        return getDocs(collection(db, "users"));
+    }
+
+    // ==> Rooms <== //
+
+    // get rooms
+    export function getRooms() {
+        return getDocs(collection(db, "rooms"));
+    }
+
+    export function getUserRoom(userID) {
+        const roomsRef = collection(db, "rooms");
+        const q = query(roomsRef, where("userID", "==", userID));
+        return getDocs(q);
+    }
+
+    // profiles
+    export function getProfiles() {
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, orderBy(`ProfileListing`));
+        return getDocs(q);
+    }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,10 +2,36 @@ import { Container, HStack, IconButton, Input, InputGroup, InputLeftElement, Sim
 import Card from '../components/Card';
 import Header from '../components/Layout/Header';
 
+import { useState, useEffect } from 'react';
+import { getRooms } from '../firestore/utils';
+
 import { BsSearch } from 'react-icons/bs';
 import { FiFilter } from 'react-icons/fi';
 
 const Rooms = () => {
+
+    const [loading, setLoading] = useState();
+    const [rooms, setRooms] = useState();
+
+    const handleGetRooms = async () => {
+        setLoading(true);
+        try {
+          const querySnapshot = await getRooms();
+          setRooms(
+            querySnapshot.docs.map( (doc) => ({ ...doc.data(), id: doc.id }))
+          );
+        } catch (error) {
+          console.log(error.message);
+        }
+        setLoading(false);
+      };
+
+    useEffect(() => {
+        handleGetRooms();
+    }, []);
+
+
+
     return ( 
         <Container maxW="container.xl" mt="24">
             <Header description="Join people who already have a place"/>
@@ -23,12 +49,18 @@ const Rooms = () => {
             </HStack>
 
             <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={5} mt={6}>
-                <Card title="Miftah ElKheir, Safi" post={"https://i.imgur.com/5BtLV52.jpeg"} />
-                <Card title="Hay Salam, Safi" post={"https://i.imgur.com/LuycVeB.jpeg"} />
-                <Card title="Jrifat, Safi" post={"https://i.imgur.com/9lXhrTh.jpeg"} />
-                <Card title="Hay Anas, Safi" post={"https://i.imgur.com/vHiYiXj.jpeg"} />
-                <Card title="Hay Ourida 2, Safi" post={"https://i.imgur.com/VTJ5oz4.jpeg"} />
-                <Skeleton borderRadius={['sm', null, 'md']} />
+                {
+                    loading ?
+                    [...Array(4)].map( (elm, i) => {
+                        return <Skeleton key={i} rounded='md' h="219px"/>
+                    })
+                    :
+                    rooms && rooms.map((doc) => {
+                        return (
+                            <Card key={doc.id} title={doc.location} post={doc.img} />
+                        );
+                    })
+                }
             </SimpleGrid>
         </Container>
      );
